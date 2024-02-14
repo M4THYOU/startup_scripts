d6367105e17a266fb9a2a762c4a079de7bec1354
Matt Wolfe <m5wolfe@uwaterloo.ca>
2024-02-14 15:09:05 -0500

    auto yes

--- base.sh
+++ base.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install build-essential
-sudo apt-get install python3-dev
-sudo apt-get install python3.11-dev
-sudo apt-get install libpq-dev
-sudo apt-get install neovim
+sudo apt-get -y install build-essential
+sudo apt-get -y install python3-dev
+sudo apt-get -y install python3.11-dev
+sudo apt-get -y install libpq-dev
+sudo apt-get -y install neovim
 curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
